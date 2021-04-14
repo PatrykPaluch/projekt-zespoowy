82729dc88bc865b5a8a0327afb4fb58c657f030e
Agnieszka <agix999r@gmail.com>
2021-04-14 09:49:08 +0200

    Added CallendarCards in view

--- frontend/src/components/Callendar/Callendar.js
+++ frontend/src/components/Callendar/Callendar.js
@@ -20,12 +20,16 @@ const Callendar = () => {
                 <CardCallendar column={3} color='#81DB7B' startRow={8} endRow={12}/>
                 <CardCallendar column={3} color='#DB7B7B' startRow={13} endRow={16}/>
 
-                <CardCallendar column={3} color='#B27BDB' startRow={4} endRow={7}/>
-                <CardCallendar column={4} color='#E0E03B' startRow={2} endRow={4}/>
-                <CardCallendar column={5} color='#B27BDB' startRow={1} endRow={4} />
+                <CardCallendar column={4} color='#B27BDB' startRow={4} endRow={7}/>
+                <CardCallendar column={4} color='#E0E03B' startRow={8} endRow={11}/>
+
+                <CardCallendar column={5} color='#B27BDB' startRow={8} endRow={11} />
+                <CardCallendar column={5} color='#E0E03B' startRow={12} endRow={15} />
+                <CardCallendar column={5} color='#DB7B7B' startRow={16} endRow={19} />
+
                 <CardCallendar column={6} color='#81DB7B' startRow={2} endRow={5}/>
-                <CardCallendar column={7} color='#7BC7DB' startRow={4} endRow={7}/>
-                <CardCallendar column={8} color='#DB7B7B' startRow={1} endRow={4}/>
+                <CardCallendar column={6} color='#7BC7DB' startRow={6} endRow={9}/>
+                <CardCallendar column={6} color='#DB7B7B' startRow={10} endRow={13}/>
             </div> 
     </div>
         
