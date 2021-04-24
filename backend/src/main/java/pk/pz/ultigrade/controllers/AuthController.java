cc7af10b39b1470de6d20c949148161d8921b948
Patryk Paluch <patrykas1000@gmail.com>
2021-04-24 17:35:53 +0200

    Optimize imports and cleanup

--- backend/src/main/java/pk/pz/ultigrade/controllers/AuthController.java
+++ backend/src/main/java/pk/pz/ultigrade/controllers/AuthController.java
@@ -2,7 +2,6 @@ package pk.pz.ultigrade.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import pk.pz.ultigrade.configurations.SecurityConfig;
