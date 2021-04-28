70c22d1b5c5ccea6307c12a9f9c42a9f97162e18
sylwia02.02.1999@interia.pl <sylwia02.02.1999@interia.pl>
2021-04-28 17:56:46 +0200

    Added axios to Profile Data

--- frontend/src/pages/subpages/Children.js
+++ frontend/src/pages/subpages/Children.js
@@ -1,73 +1,62 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ProfileNav from '../../components/ProfileNav';
 import Navbar from "../../components/Navbar";
 import './Profile.css';
 import photo from '../../image/avatar.png'; 
+import axios from "axios";
 
 function Children() {
+    const [children,setChildren] = useState({
+        kids: []
+    });
+
+    function getChildren() {
+        axios.get(`https://jsonplaceholder.typicode.com/users`).then(response =>{
+            setChildren({kids:response.data});
+            console.log(children);
+        });
+    }
+
+    useEffect(() =>{
+        getChildren();
+    }, []);
+
     return (
        <>
         <Navbar/>
         <ProfileNav/>
         <div className="Children">
-        <div className="User-data">
-                <div className="Data">
-                    <div className="Data-item">
-                        <h4>Imię</h4> 
-                        <h3>Jan </h3>
+            {children.kids.map(child=> (
+                <div className="User-data">
+                    <div className="Data">
+                        <div className="Data-item">
+                            <h4>Imię</h4> 
+                            <h3>{child.name} </h3>
+                        </div>
+                        <div className="Data-item">
+                            <h4>Nazwisko</h4> 
+                            <h3>{child.surname}</h3>
+                        </div>
+                        <div className="Data-item">
+                            <h4>Pesel</h4> 
+                            <h3>{child.pesel}</h3>
+                        </div>
+                        <div className="Data-item">
+                            <h4>Data urodzenia</h4> 
+                            <h3>{child.dateOfBirth}</h3>
+                        </div>
+                        <div className="Data-item">
+                            <h4>Klasa</h4> 
+                            <h3>{child.class}</h3>
+                        </div>
                     </div>
-                    <div className="Data-item">
-                        <h4>Nazwisko</h4> 
-                        <h3>Kowalski </h3>
-                    </div>
-                    <div className="Data-item">
-                        <h4>Pesel</h4> 
-                        <h3>89030267456 </h3>
-                    </div>
-                    <div className="Data-item">
-                        <h4>Data urodzenia</h4> 
-                        <h3>04.06.2005</h3>
-                    </div>
-                    <div className="Data-item">
-                        <h4>Klasa</h4> 
-                        <h3>1b</h3>
-                    </div>
+                    <div className="Photo">
+                        <div className="Data-item">
+                        <img className="Photo-avatar" src={photo} alt={"User avatar"}/>
+                        </div>
+                    </div>  
                 </div>
-                <div className="Photo">
-                    <div className="Data-item">
-                    <img className="Photo-avatar" src={photo} alt={"User avatar"}/>
-                    </div>
-                </div>
-            </div>
-            <div className="User-data">
-                <div className="Data">
-                    <div className="Data-item">
-                        <h4>Imię</h4> 
-                        <h3>Jan </h3>
-                    </div>
-                    <div className="Data-item">
-                        <h4>Nazwisko</h4> 
-                        <h3>Kowalski </h3>
-                    </div>
-                    <div className="Data-item">
-                        <h4>Pesel</h4> 
-                        <h3>89030267456 </h3>
-                    </div>
-                    <div className="Data-item">
-                        <h4>Data urodzenia</h4> 
-                        <h3>04.06.2005</h3>
-                    </div>
-                    <div className="Data-item">
-                        <h4>Klasa</h4> 
-                        <h3>1b</h3>
-                    </div>
-                </div>
-                <div className="Photo">
-                    <div className="Data-item">
-                    <img className="Photo-avatar" src={photo} alt={"User avatar"}/>
-                    </div>
-                </div>
-            </div>
+            ))}
         </div>
        </>
     )
