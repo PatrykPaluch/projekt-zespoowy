9cb187f62346108a132ab315a42365770b996e87
sylwia02.02.1999@interia.pl <sylwia02.02.1999@interia.pl>
2021-04-30 14:23:28 +0200

    Added axios to messages

--- frontend/src/pages/subpages/NewMessage.js
+++ frontend/src/pages/subpages/NewMessage.js
@@ -1,17 +1,77 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MessageNav from '../../components/MessageNav';
 import './NewMessage.css';
 import Navbar from "../../components/Navbar";
+import axios from "axios";
+import {useForm} from "react-hook-form";
 
 function NewMessage() {
+    const {register, handleSubmit} = useForm();
+
+    const [user, setUser] = useState({
+        currentUserId: 1
+    });
+
+    // useEffect(() => {
+    //     axios.get('http://localhost:8000/currentUser').then(response => {
+    //             setUser({
+    //                  currentUserId:response.data[0].id
+    //          });
+    //          console.log(user);
+    //         });
+    // }, []);
+
+    let tmp = ['Sylwia Rusek', 'Marcin Poręba', 'Agnieszka Rejczak'];
+    // useEffect(() => {
+    //     let tmpReceivers = [];
+    //     axios.get('url to get receivers')
+    //         .then(response => {
+    //             response.forEach(s => {
+    //                 tmpReceivers.push(s);
+    //             })
+    //         })
+    //     setReceivers(tmpReceivers);
+    // })
+
+    const onSubmit = formData => {
+        console.log(formData);
+        axios.post(`http://localhost:8000/sendMessage`, {
+            currentUserId: user.currentUserId,
+            receiverUserNameSurname: onSubmit.receiverUserNameSurname,
+            topic: onSubmit.topic,
+            contents: onSubmit.contents
+        }).then(function (response) {
+            console.log(response);
+        }).catch(function (error) {
+            console.log(error);
+        });
+    };
+
     return (
        <>
         <Navbar/>
         <MessageNav/>
-            <form className="message-form">
-                <input className="receiver" placeholder="Do"></input>
-                <input className="message-title" placeholder="Temat"></input>
-                <input className="message-input" placeholder="Wiadomość"></input>
+            <form className="message-form" onSubmit={handleSubmit(onSubmit)}>
+                <select 
+                    className="receiver" 
+                    {...register("receiverUserNameSurname")}>
+                        {/*todo instead of tmp receivers*/}
+                    {tmp.map((receiverUserNameSurname, key) => {
+                        return <option value={receiverUserNameSurname} key={key}>{receiverUserNameSurname}</option>
+                    })}
+                </select>
+                <input 
+                    className="message-title" 
+                    placeholder="Temat"
+                    {...register("topic")}
+                />
+                <input 
+                    className="message-input" 
+                    placeholder="Wiadomość"
+                    name="contents"
+                    type="text"
+                    {...register("contents")}
+                />
                 <button className="send" type="submit"> Wyślij wiadomość</button>
             </form>
        </>
