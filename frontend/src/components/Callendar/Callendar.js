527461b6312db46f1024289991e435cac96863b5
Agnieszka <agix999r@gmail.com>
2021-05-14 08:43:41 +0200

    first

--- frontend/src/components/Callendar/Callendar.js
+++ frontend/src/components/Callendar/Callendar.js
@@ -5,14 +5,20 @@ import Hours from './Hours'
 import './callendar.css';
 
 const Callendar = () => {
+    let cardCallExample ={
+        dayOfWeek: "Monday",
+        time:"08:00"
+    };
     return (
     <div className='callendar'>
             <HeaderDays/>
             <div className='callendar-inner'>
                 <Hours/>
                 {/* TODO passing proper value to CardCallendar (id, date, time, school subject) */}
-                <CardCallendar column={2} color= 'rgb(255,255,255)' startRow={1} endRow={4}/>
-                <CardCallendar column={2} color='#7B84DB' startRow={5} endRow={8}/>
+                
+
+                <CardCallendar dayOfWeek="Wednesday" subject='Język Polski' time='10:00' color= 'hsl(213, 100%, 76%)'/>
+                {/* <CardCallendar column={2} color='#7B84DB' startRow={5} endRow={8}/>
                 <CardCallendar column={2} color='#7BC7DB' startRow={9} endRow={12}/>
                 <CardCallendar column={2} color='#81DB7B' startRow={13} endRow={16}/>
 
@@ -29,7 +35,7 @@ const Callendar = () => {
 
                 <CardCallendar column={6} color='#81DB7B' startRow={2} endRow={5}/>
                 <CardCallendar column={6} color='#7BC7DB' startRow={6} endRow={9}/>
-                <CardCallendar column={6} color='#DB7B7B' startRow={10} endRow={13}/>
+                <CardCallendar column={6} color='#DB7B7B' startRow={10} endRow={13}/> */}
             </div> 
     </div>
         
--- frontend/src/components/Callendar/CardCallendar.js
+++ frontend/src/components/Callendar/CardCallendar.js
@@ -1,11 +1,40 @@
 import React from 'react'
 
 const CardCallendar = (props) => {
+    let colorPool=[];
+    
+    let calcHour = (hour) =>{
+        let hourNumber =hour.split(':')[0];
+        hourNumber[0]==='0' && hourNumber.replace('0','');
+        let rowNumber = (hourNumber-7)*4 +1;
+        return rowNumber
+    };
+
+    let columnCheck= (day) =>{
+        switch (day){
+            case "Monday" :
+                return 2;
+            case "Tuesday" :
+                return 3;
+            case "Wednesday" :
+                return 4;
+            case "Thursday" :
+                return 5;
+            case "Friday" :
+                return 6;
+            default:
+                console.log("Wrong day name!");
+                break;
+        }
+    };
+    
+
     return (
         <div className='card-callendar' style={{
-            gridColumnStart:props.column, backgroundColor: props.color, gridRowStart: props.startRow, gridRowEnd: props.endRow}}>
-            <h5>Title</h5>
-            <h6>7:30-10:30</h6>
+            gridColumnStart:columnCheck(props.dayOfWeek), backgroundColor: props.color, gridRowStart: calcHour(props.time), gridRowEnd: calcHour(props.time)+4}}>
+            <h5>{props.subject}</h5>
+            <h6>{props.time} - {props.time.split(':')[0]}:45</h6>
+            
         </div>
     )
 }
